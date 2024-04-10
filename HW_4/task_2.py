e295c64e37314484993f4e7bbea67ec27b341a19
PerevoznicVladislav <vladperevoznic@gmail.com>
2024-04-10 17:19:17 +0300

    Add HW_Leson_4

---
+++ HW_4/task_2.py
@@ -0,0 +1,134 @@
+# Aceasta este a doua ta sarcină legată a leciei legate de stringuri în python
+
+# Creează o variabilă numită `name` și seteaz-o la numele tău
+
+# CODUL TĂU VINE MAI JOS:
+name = 'Vlad'
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează valoarea variabilei `name` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(name)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creează o nouă variabilă numită `name2` și seteaz-o la valoarea variabilei `name` și afișează valoarea variabilei `name2` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+name2 = name
+print("Valoarea variabilei name2 este:", name2)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează ultimul caracter al variabilei `name` folosind indexarea
+
+# CODUL TĂU VINE MAI JOS:
+print("Ultimul caracter a variabilei name este:",name[-1])
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează primele 3 caractere ale variabilei `name` folosind slicing
+
+# CODUL TĂU VINE MAI JOS:
+print("Primele 3 caractere sunt:",name[:3])
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează valoarea variabilei `name` în majuscule folosind metoda `upper`
+
+# CODUL TĂU VINE MAI JOS:
+print("Caracterele in majuscule:",name.upper())
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează valoarea variabilei `name` în minuscule folosind metoda `lower`
+
+# CODUL TĂU VINE MAI JOS:
+print("Caracterele in minuscule",name.lower())
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează concatenarea variabilelor `name` și `name2`
+
+# CODUL TĂU VINE MAI JOS:
+print(name + name2)
+# CODUL TĂU VINE MAI SUS:
+
+# Creează o variabilă `text` și setează-i un text la alegere, cu restricția ca acesta să conțină mai multe rânduri
+
+# CODUL TĂU VINE MAI JOS:
+text = 'Salut phyton\nSalut Vlad'
+print(text)
+# CODUL TĂU VINE MAI SUS:
+
+# Verifică dacă variabila `text` conține cuvântul `python`
+
+# CODUL TĂU VINE MAI JOS:
+if 'phyton' in text:
+    print('Variabila text contine cuvantul phyton')
+else:
+    print('Variabila text nu contine cuvantul phyton')
+# Sau alta metoda
+print('phyton' in text) 
+# CODUL TĂU VINE MAI SUS:
+
+# Folosește metoda replace pentru a înlocui litera `a` din variabila `text` cu litera `e`
+
+# CODUL TĂU VINE MAI JOS:
+print(text.replace('a', 'e'))
+# CODUL TĂU VINE MAI SUS:
+
+# Folosește metoda split pentru a transforma variabila `text` într-o listă de cuvinte
+
+# CODUL TĂU VINE MAI JOS:
+print(text.split(" "))
+# CODUL TĂU VINE MAI SUS:
+
+# Creează un f-string care să conțină variabilele `name` și `name2`
+
+# CODUL TĂU VINE MAI JOS:
+f_string = f'Numele primului particiant este {name}, iar numele urmatorului la fel este {name2}!'
+print(f_string)
+# CODUL TĂU VINE MAI SUS:
+
+# Verifică dacă string-ul creat se termină cu `!`
+
+# CODUL TĂU VINE MAI JOS:
+print('!' in f_string)
+# CODUL TĂU VINE MAI SUS:
+
+# Verifică dacă string-ul creat începe cu `Hello`
+
+# CODUL TĂU VINE MAI JOS:
+print(f_string.startswith('Hello'))
+# CODUL TĂU VINE MAI SUS:
+
+# Identifică indexul unde se află `!` în string-ul creat
+
+# CODUL TĂU VINE MAI JOS:
+print(f_string.index('!'))
+print(f_string[77]) # Control aditional
+# CODUL TĂU VINE MAI SUS:
+
+# Identifică indexul unde se află `o` în string-ul creat folosind altă metodă
+
+# CODUL TĂU VINE MAI JOS:
+print(f_string.find('o'))
+# CODUL TĂU VINE MAI SUS:
+
+# Utilizând format string-uri, creează un string care să conțină variabilele `name` și `name2`
+
+# CODUL TĂU VINE MAI JOS:
+name3 = name + name2
+# CODUL TĂU VINE MAI SUS:
+
+# Concatenează string-ul creat cu string-ul `text`
+
+# CODUL TĂU VINE MAI JOS:
+name3 += text
+print('Valoarea pt variabuila name3:',name3) #name = Vlad, name2 = Vlad, text = Salut phyton\n Salut Vlad
+# CODUL TĂU VINE MAI SUS:
+
+# Afișează lungimea string-ului creat
+
+# CODUL TĂU VINE MAI JOS:
+print(len(name3))
+# CODUL TĂU VINE MAI SUS:
+
+# Aceasta a fost tot pentru această sarcină
+
