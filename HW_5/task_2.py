88c0d2f2f1219912014c860f9ab5b142ae7957ee
PerevoznicVladislav <vladperevoznic@gmail.com>
2024-04-17 12:22:46 +0300

    Add_HW_Lesson_5

---
+++ HW_5/task_2.py
@@ -0,0 +1,83 @@
+# Aceasta este a doua sarcină legată de tuples
+
+# Creează un tuple numit `numbers` care să conțină numerele de la 1 la 5
+
+# CODUL TĂU VINE MAI JOS:
+numbers = (1, 2, 3, 4, 5)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+print("Tuplul creat este:", numbers)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum adaugă numărul 6 la tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+numbers = numbers + (6,)
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+print("Adaugarea elementului 6:", numbers)
+# CODUL TĂU VINE MAI SUS:
+
+# Afișeați primul element din tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+print("Primul element din tuplu este:", numbers[0])
+# CODUL TĂU VINE MAI SUS:   
+
+# Afișeați ultimul element din tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+print("Ultimmul element din tuplu este:", numbers[-1])
+# CODUL TĂU VINE MAI SUS:
+
+# Afișeați primele două elemente din tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+print("Primele doua elemente din tuplu sunt:", numbers[:2])
+# CODUL TĂU VINE MAI SUS:
+
+# Afișeați ultimele două elemente din tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+print("Ultimele doua elemente din tuplu sunt:", numbers[-2:])
+# CODUL TĂU VINE MAI SUS:
+
+# Afișați lungimea tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+print("Lungimea tuplului este:", len(numbers))
+# CODUL TĂU VINE MAI SUS:
+
+# Afișați suma elementelor din tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+print("Suma elementelor din tuplu este:", sum(numbers))
+# CODUL TĂU VINE MAI SUS:
+
+# Schibați elementul de la poziția 2 din tuple `numbers` cu 10
+
+# CODUL TĂU VINE MAI JOS:
+numbers_list = list(numbers)
+numbers_list[2] = 10
+numbers = tuple(numbers_list)
+# CODUL TĂU VINE MAI SUS:
+
+# Afișați tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+print("Tuplul schimbat este:", numbers)
+# CODUL TĂU VINE MAI SUS:
+
+# Ștergeți tuple `numbers`
+
+# CODUL TĂU VINE MAI JOS:
+del(numbers)
+# CODUL TĂU VINE MAI SUS:
+
+# Asta a fost tot, ai terminat prima ta sarcină legată de liste
